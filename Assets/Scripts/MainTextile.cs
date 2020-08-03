2f85f336026116f383f983618059ad5bd7551e0e
Laura <laamorenoro@unal.edu.co>
2020-08-02 23:10:07 -0500

    Add rotative machine scripts

---
+++ Assets/Scripts/MainTextile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainTextile : MonoBehaviour
+{
+    public Transform rotTable;
+    public Transform roller1, roller2, roller3;
+
+    RotTable rotativeMachine;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rotativeMachine = new RotTable(rotTable, roller1, roller2, roller3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       rotativeMachine.TurnON();
+    }
+}
--- Assets/Scripts/RotTable.cs
+++ Assets/Scripts/RotTable.cs
@@ -3,88 +3,133 @@ using UnityEngine;
 
 public class RotTable : MonoBehaviour
 {
-    public Transform Stamp1;
-    public Transform Stamp2;
-    public Transform Stamp3;
-
+    private Transform model;
     private float tiltAroundZ;
-    private bool bRotate, bFirstTime, bIsUp;
-    public float rotate;
-    public float timer;
-    public float waitingTime;
+    private bool bRotate, bMoveUpDown, bIsUp, bPaint;
+    private float rotate, timer, waitingTime, paintTime;
+    private Vector3 maxPos, minPos;
 
-    private Vector3 maxPos, minPos; 
-    
+    private Transform roll1, roll2, roll3;
+    private Roller roller1, roller2, roller3; 
+
     // Start is called before the first frame update
-    void Start()
+    public RotTable(Transform t, Transform r1, Transform r2, Transform r3)
     {
+        model = t;
+        roll1 = r1;
+        roll2 = r2;
+        roll3 = r3;
+
         tiltAroundZ = 0.5f;
         rotate = 0f;
-        waitingTime = 5f;
+        SetSpeed(0);
         timer = 0f;
-        maxPos = new Vector3(Stamp1.localPosition.x, Stamp1.localPosition.y, -0.06f);
-        minPos = Stamp1.localPosition;
+        maxPos = new Vector3(model.localPosition.x, 0.7f, model.localPosition.z );
+        minPos = model.localPosition;
 
         bRotate = true;
-        bFirstTime = true;
-        bIsUp = true; 
+        bMoveUpDown = false;
+        bIsUp = false;
+
+        roller1 = new Roller(roll1, 0.01359491f, -0.0051f);
+        roller2 = new Roller(roll2, 0.0084f, -0.009359201f);
+        roller3 = new Roller(roll3, 0.0050f, -0.01365004f);
+
+        paintTime = 2;
+        bPaint = false;
     }
 
+    public void SetSpeed(int speed)
+    {
+        switch (speed)
+        {
+            case 1:
+                waitingTime = 8;
+                break;
+            case 2:
+                waitingTime = 4;
+                break;
+            case 3:
+                waitingTime = 2;
+                break;
+            default:
+                waitingTime = 2;
+                break;
+        }
+    }
     // Update is called once per frame
-    void Update()
+    public void TurnON()
     {
         if (bRotate)
         {
             rotate += tiltAroundZ;
-            transform.Rotate(0, 0, tiltAroundZ);
+            model.Rotate(0, 0, tiltAroundZ);
             if (rotate == 90)
             {
                 rotate = 0f;
                 bRotate = false;
+                bMoveUpDown = true;
             }
         }
         else
         {
-            if (bFirstTime)
+            if (bMoveUpDown)
             {
                 if (bIsUp)
-                    ArmDown();
-                else
-                    ArmUp();
-            }
+                {
+                    if (bPaint)
+                    {
+                        roller1.Paint();
+                        //roller2.Paint();
+                        roller3.Paint();
 
-            timer += Time.deltaTime;
-            if (timer > waitingTime)
-            {
-                timer = 0f;
-                bRotate = true;
-                bFirstTime = true;
+                        timer += Time.deltaTime;
+                        if (timer > paintTime)
+                        {
+                            timer = 0f;
+                            bPaint = false;
+                        }
+                    }
+                    else 
+                        Down();
+                }
+                else
+                    Up();
+            }
+            else
+            {   //waiting time in down position
+                timer += Time.deltaTime;
+                if (timer > waitingTime)
+                {
+                    timer = 0f;
+                    bRotate = true;
+                    bMoveUpDown = false;
+                }
             }
         }
     }
 
-    void ArmDown()
+    void Down()
     {
-        Stamp1.Translate(Vector3.left * Time.deltaTime);
-        Stamp2.Translate(Vector3.left * Time.deltaTime);
-        Stamp3.Translate(Vector3.left * Time.deltaTime);
-        Vector3 localPos = Stamp1.localPosition;
+        model.Translate(Vector3.back * Time.deltaTime * 2);
+        Vector3 localPos = model.localPosition;
 
-        if (localPos.z < maxPos.z)
-            bIsUp = false;
-    }
-    void ArmUp()
-    {        
-        Stamp1.Translate(Vector3.right * Time.deltaTime);
-        Stamp2.Translate(Vector3.right * Time.deltaTime);
-        Stamp3.Translate(Vector3.right * Time.deltaTime);
-
-        Vector3 localPos = Stamp1.localPosition;
-
-        if (localPos.z > minPos.z)
+        if (localPos.y < minPos.y)
         {
-            bIsUp = true; 
-            bFirstTime = false;
+            bIsUp = false;
+            bMoveUpDown = false;
+        }
+    }
+    void Up()
+    {
+        model.Translate(Vector3.forward * Time.deltaTime * 2);
+        
+        Vector3 localPos = model.localPosition;
+
+        if (localPos.y > maxPos.y)
+        {
+            bIsUp = true;
+            bPaint = true; 
         }
     }
 }
